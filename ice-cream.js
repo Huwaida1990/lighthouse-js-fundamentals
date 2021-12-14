ecd8512602dec98c0e4900c60e6d3a6a7c32c663
Huwaida1990 <huwaida_khaild@yahoo.com>
2021-12-14 10:06:35 -0700

    ice_cream

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+console.log(iceCreamFlavours);
+// Your code below here...
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours);
+console.log("chocolate");
+console.log("root beer");
+console.log(iceCreamFlavours.length);
